3accee6811bda31f66d47c7a2c3ea1d61c26566d
ahmadriyad46@gmail.com <ahmadriyad46@gmail.com>
2022-04-18 21:35:51 +0700

    project-1

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-<<<<<<< HEAD
 const port = 4000;
 const usersRoute = require('./routes/user')
 
@@ -12,18 +11,3 @@ app.use('/api/v1', usersRoute)
 app.listen(port, function () {
     console.log('Contoh server berjalan di port 4000!')
 })
-=======
-const port = process.env.PORT || 3000
-const router = require('./routes/app.route')
-
-appInit(app)
-
-app.listen(port, () => {
-    console.log(`App listening on http://localhost:${port}`)
-})
-
-function appInit(app) {
-    app.use(express.json())
-    app.use('/', router)
-}
->>>>>>> b12a60f0ee577b2b617c118fd93c9a511d80ceb4
